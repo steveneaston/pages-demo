fd2a1284def8fc5f6abde538ea9543143faf0d56
Steven Easton <steven@amiqus.co>
2023-08-21 15:38:22 +0100

    Add Google fonts

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -89,6 +89,20 @@ export default withMermaid(
         linkText: 'Back home'
       }
     },
+    head: [
+      [
+        'link',
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' }
+      ],
+      [
+        'link',
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }
+      ],
+      [
+        'link',
+        { href: 'https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital@0;1&family=Source+Sans+3:ital,wght@0,400;0,600;1,400;1,600&display=swap', rel: 'stylesheet' }
+      ]
+    ],
     mermaid: {
       // Mermaid.js config
     },
